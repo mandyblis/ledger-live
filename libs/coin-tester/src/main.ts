7d0d76278f62f9eeeeed8f276592c4ac80275fa1
lvndry <lvndry@proton.me>
2024-04-23 19:32:29 +0200

    add expects in polygon test

--- libs/coin-tester/src/main.ts
+++ libs/coin-tester/src/main.ts
@@ -168,6 +168,7 @@ export async function executeScenario<T extends TransactionCommon>(scenario: Sce
             ),
           );
         }
+      };
 
         try {
           testTransaction.expect?.(previousAccount, scenarioAccount);
